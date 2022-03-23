346d45111fca4d58bf90a195c53e4d8e6a07a2f6
Ted Gelpi <tgelpi@gmail.com>
2022-03-23 16:51:04 -0400

    Update jenkinsJob.groovy

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -23,7 +23,7 @@ def packageArtifact(){
 def buildAndTest(){
     stage("Backend tests") {
         withEnv(["PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"]) {
-        sh mvn test"
+        sh "mvn test"
         }
     }
 }
